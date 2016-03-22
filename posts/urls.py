ee8d21ddee2d7fc3705bd610a7f5768eb5683cc5
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-03-22 22:17:03 +0100

    templates and views

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import patterns, url
-from posts import views
+
+from .views import (
+	posts_list,
+	posts_create,
+	posts_detail,
+	posts_update,
+	posts_delete,
+	)
+
+
+
 
 urlpatterns = [
-	url(r'^$', "posts.views.posts_home"),
+	url(r'^$', posts_list),
+	url(r'^create/$', posts_create),
+	url(r'^detail/$', posts_detail),
+	url(r'^update/$', posts_update),
+	url(r'^delete/$', posts_delete),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
-def posts_home(request):
+def posts_create(request):
 
-	return HttpResponse("<h1>Hello</h1>")
+	return HttpResponse("<h1>Create</h1>")
+
+def posts_detail(request):
+	context = {
+		"title": "Detail"
+	}
+
+	return render(request, "index", context)
+
+def posts_list(request):
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "List"
+	}
+	return render(request, "index.html", context)
+
+def posts_update(request):
+
+	return HttpResponse("<h1>Update</h1>")
+
+def posts_delete(request):
+
+	return HttpResponse("<h1>Delete</h1>")
