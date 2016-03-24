97db7cdd04ecc9c4922177efa6c7fe60f8ef58e2
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-03-24 22:28:15 +0100

    Slugfield

--- posts/views.py
+++ posts/views.py
@@ -21,8 +21,8 @@ def post_create(request):
 	}
 	return render(request, "post_form.html", context)
 
-def post_detail(request, id = None):
-	instance =get_object_or_404(Post, id =id)
+def post_detail(request, slug = None):
+	instance =get_object_or_404(Post, slug =slug)
 	context = {
 		"title": instance.title,
 		"instance":instance,
@@ -38,8 +38,8 @@ def post_list(request):
 	}
 	return render(request, "post_list.html", context)
 
-def post_update(request, id=None):
-	instance = get_object_or_404(Post, id =id)
+def post_update(request, slug=None):
+	instance = get_object_or_404(Post, id =slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -55,8 +55,8 @@ def post_update(request, id=None):
 	return render(request, "post_form.html", context)
 	
 
-def post_delete(request, id=None):
-	instance = get_object_or_404(Post, id =id)
+def post_delete(request, slug=None):
+	instance = get_object_or_404(Post, slug =slug)
 	messages.success(request, "Successfully Deleted")
 	instance.delete()
 	return redirect("posts:list")
