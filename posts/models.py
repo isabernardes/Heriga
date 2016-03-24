6f4de0eec4a80dbb76762d13cb1f4eabd278cd8c
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-03-24 20:28:05 +0100

    Imagefield and pillow

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
+def upload_location(instance, filename):
+	return "%s/%s" %(instance.id, filename)
+
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-	image = models.FileField(null=True, blank=True)
+	image = models.ImageField(upload_to=upload_location,
+			null=True, 
+			blank=True,
+			width_field = "width_field",
+			height_field = "height_field") 
+	height_field = models.IntegerField(default=0)
+	width_field = models.IntegerField(default=0)
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
