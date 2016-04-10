a901528a6577376e36b4d50167f7f9b00ec30b6b
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-04-11 00:55:26 +0200

    static

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -131,11 +131,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "extra_static"),
+    os.path.join(os.path.join(BASE_DIR, "extra_static")),
     #'/var/www/static/',
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR, "static/"))
+MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR, "media/"))
 
