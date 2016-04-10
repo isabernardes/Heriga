33d9127f4884dcf3f4a4f3b97f90000341b00159
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-04-10 20:57:31 +0200

    add requirements

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -17,3 +17,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
+
+
+try:
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+except:
+	pass
+ 
