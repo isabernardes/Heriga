319b42de07eb57d2704211e2af4521ef5f16db8f
isabernardes <isabelbernardes@Isabels-MacBook-Air.local>
2016-04-11 17:41:55 +0200

    wgsi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -19,10 +19,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
 
 
-try:
-	from dj_static import Cling
 
-	application = Cling(get_wsgi_application())
-except:
-	pass
+from dj_static import Cling
+application = Cling(get_wsgi_application())
+
  
